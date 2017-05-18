bfd6148aff04992e785da38a60efacd4b170bdf3
Winter Alexander <1541869@cegepmontpetit.ca>
2017-05-18 08:42:24 -0400

    Added test

--- src/test/java/me/winter/boing/simulation/BoxStackSimulation.java
+++ src/test/java/me/winter/boing/simulation/BoxStackSimulation.java
@@ -96,6 +96,43 @@ public class BoxStackSimulation
 	{
 		TestWorldImpl world = new TestWorldImpl(new ReplaceResolver());
 
+		GravityAffected test = new GravityAffected();
+		test.getPosition().set(400, 500);
+		test.addCollider(new Box(test, 0, 0, 30, 30));
+		world.add(test);
+
+		PlayerImpl player = new PlayerImpl();
+
+		player.getPosition().set(400, 600);
+		player.addCollider(new Box(player, 0, 0, 20, 45));
+		world.add(player);
+
+
+
+		GravityAffected test2 = new GravityAffected();
+		test2.getPosition().set(400, 400);
+		test2.addCollider(new Box(test2, 0, 0, 30, 30));
+		world.add(test2);
+
+		GravityAffected test3 = new GravityAffected();
+		test3.getPosition().set(400, 300);
+		test3.addCollider(new Box(test3, 0, 0, 50, 50));
+		world.add(test3);
+
+		BodyImpl ground = new BodyImpl();
+
+		ground.getPosition().set(400, 0);
+		ground.addCollider(new Box(ground, 0, 0, 800, 400));
+		world.add(ground);
+
+		WorldSimulationUtil.simulate(world);
+	}
+
+	@Test
+	public void bigBoxStackWithLazyFloatyBox()
+	{
+		TestWorldImpl world = new TestWorldImpl(new ReplaceResolver());
+
 		GravityAffected test = new GravityAffected() {
 			@Override
 			public void update(float delta)
