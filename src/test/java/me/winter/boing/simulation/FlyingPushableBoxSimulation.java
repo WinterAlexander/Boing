3915c30795867997d9742ee26463fffbe0538ba1
Alexander Winter <a.w1nter@hotmail.com>
2017-05-22 17:25:49 -0400

    Added simulation for the box stack effect without gravity

--- src/test/java/me/winter/boing/simulation/FlyingPushableBoxSimulation.java
+++ src/test/java/me/winter/boing/simulation/FlyingPushableBoxSimulation.java
@@ -65,4 +65,38 @@ public class FlyingPushableBoxSimulation
 
 		WorldSimulationUtil.simulate(testWorld);
 	}
+
+	@Test
+	public void lotsOfBoxesWithLimits()
+	{
+		TestWorldImpl testWorld = new TestWorldImpl(new ReplaceResolver());
+
+		FlyingPlayerImpl player = new FlyingPlayerImpl();
+		player.getPosition().set(500, 400);
+		player.addCollider(new Box(player, 0, 0, 50, 50));
+		testWorld.add(player);
+
+		for(int i = 0; i < 10; i++)
+		{
+			for(int j = 0; j < 5; j++)
+			{
+				DynamicBodyImpl solid = new DynamicBodyImpl();
+				solid.getPosition().set(i * 50 + 25, j * 50 + 25);
+				solid.addCollider(new Box(solid, 0, 0, 20, 20));
+				testWorld.add(solid);
+			}
+		}
+
+		BodyImpl limit1 = new BodyImpl();
+		limit1.getPosition().set(600, 400);
+		limit1.addCollider(new Limit(limit1, 0, 0, LEFT, 40));
+		testWorld.add(limit1);
+
+		BodyImpl limit2 = new BodyImpl();
+		limit2.getPosition().set(400, 200);
+		limit2.addCollider(new Limit(limit2, 0, 0, UP, 50));
+		testWorld.add(limit2);
+
+		WorldSimulationUtil.simulate(testWorld);
+	}
 }
