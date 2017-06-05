4a1e0c2420b6d941f7e0725c759f0e0df708c8a6
Alexander Winter <a.w1nter@hotmail.com>
2017-06-05 16:09:58 -0400

    Added test showing that prev pos can't work with colliding limits moving together

--- src/test/java/me/winter/boing/simulation/StepByStepSimulation.java
+++ src/test/java/me/winter/boing/simulation/StepByStepSimulation.java
@@ -56,4 +56,40 @@ public class StepByStepSimulation
 
 		new WorldSimulation(world, 1f).start(true);
 	}
+
+	@Test
+	public void pushChain()
+	{
+		TestWorldImpl world = new TestWorldImpl(new ReplaceResolver());
+
+		DynamicBodyImpl pusher = new DynamicBodyImpl(5f);
+		pusher.getPreviousPosition().set(0, 100);
+		pusher.getPosition().set(0, 100);
+		pusher.getVelocity().set(30, 0);
+		pusher.addCollider(new Limit(pusher, 0, 0, RIGHT, 100));
+		pusher.getColliders()[0].setTag("PUSHER");
+		world.add(pusher);
+
+		DynamicBodyImpl pushed = new DynamicBodyImpl();
+
+		pushed.getPreviousPosition().set(100, 100);
+		pushed.getPosition().set(100, 100);
+		pushed.addCollider(new Limit(pushed, -50, 0, LEFT, 100));
+		pushed.addCollider(new Limit(pushed, 50, 0, RIGHT, 100));
+
+		pushed.addCollider(new Limit(pushed, 0, -50, DOWN, 100));
+		pushed.addCollider(new Limit(pushed, 0, 50, UP, 100));
+
+		world.add(pushed);
+
+		DynamicBodyImpl pushed2 = new DynamicBodyImpl();
+		pushed2.getPreviousPosition().set(220, 100);
+		pushed2.getPosition().set(220, 100);
+		pushed2.addCollider(new Limit(pushed2, 0, 0, LEFT, 100));
+		pushed2.getColliders()[0].setTag("PUSHED2");
+		world.add(pushed2);
+
+
+		new WorldSimulation(world, 1f).start(true);
+	}
 }
