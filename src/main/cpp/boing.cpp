bff783745ea541bf3c099338ff79c2c3da6ba84a
Alexander Winter <a.w1nter@hotmail.com>
2018-09-21 17:03:13 -0400

    Added cpp project

---
+++ src/main/cpp/boing.cpp
@@ -0,0 +1,6 @@
+#include <iostream> 
+
+int main(size_t argc, char** argv) {
+	std::cout << "Hello world" << std::endl;
+	return 0;
+}
---
+++ src/test/cpp/test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Alexander Winter on 2017-09-18.
+//
+
+#include <gtest/gtest.h>
+
+TEST(trivial, trivial_one_equal_one)
+{
+    ASSERT_EQ(1, 1);
+}
