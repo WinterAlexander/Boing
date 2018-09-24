09e1b6a4fdb508daf6442daa632b40ac97989f90
Alexander Winter <a.w1nter@hotmail.com>
2018-09-24 13:23:42 -0400

    Less retarded cmakelists file

--- src/main/cpp/boing.cpp
+++ src/main/cpp/boing.cpp
@@ -6,8 +6,8 @@ int main(int argc, char** argv) {
 	boing::vec2 vec(3, 1);
 	boing::vec2 vec2(3, -4);
 
-	std::cout << vec << std::endl;
-	std::cout << vec2 << std::endl;
+	//std::cout << vec << std::endl;
+	std::cout << "test" << std::endl;
 
 	return 0;
 }
